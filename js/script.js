ec44aa754bf3db1282457aeceb7dc4d7cb215b02
Alessandro2014 <alessandromerenda22@gmail.com>
2021-09-01 17:11:55 +0200

    stampa email

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
-  methods: {},
+  data: {
+    mail: '',
+  },
+  methods: {
+  },
+  created() {
+    axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((res) => {
+        this.mail = res.data.response;
+        ;
+      });
+  },
  
 });
